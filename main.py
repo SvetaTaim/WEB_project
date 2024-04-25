c3be7b8f0bcc10d2ac86f8f9ab13f53053537967
protein <taimanova.sveta@gmail.com>
2024-04-25 21:27:25 +0300

    Сделан шаблон для окна входа

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ def home():
 def about():
     return render_template('about.html', title='О нас')
 
+
+@app.route('/registrar')
+def registrar():
+    return render_template('registrar.html', title='Регистрация')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
