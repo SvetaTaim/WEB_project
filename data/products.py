c2de09ccda76a2b87b353513ab366746b49a8964
protein <taimanova.sveta@gmail.com>
2024-04-27 01:49:53 +0300

    Ура!! Сделала БД, реистрацию и вход

---
+++ data/products.py
@@ -0,0 +1,18 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Product(SqlAlchemyBase):
+    __tablename__ = 'products'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    photos = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    category_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("category.id"))
+    category = orm.relationship('Category')
+    users = orm.relationship("User", secondary="association", backref="users")
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import association_table
+from data.users import User
+from data.products import Product
+from forms.user import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def index():
+    return redirect("/home")
 
 @app.route('/home')
 def home():
@@ -14,19 +32,46 @@ def about():
     return render_template('about.html', title='О нас')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Вход')
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            return redirect("/home")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Вход', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
-@app.route('/register')
-def register():
-    return render_template('register.html', title='Регистрация')
-
-
-@app.route('/<username>')
+@app.route('/account')
 def user(username):
-    return render_template('user.html', title='Вход', name=username)
+    return render_template('account.html', title='Аккаунт')
 
 
 @app.route('/contact')
@@ -34,10 +79,11 @@ def contact():
     return render_template('contact.html', title='Контакты')
 
 
-@app.route('/category')
-def category():
-    return render_template('category.html', title='Категории')
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html', title='Каталог')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    db_session.global_init("db/shop.db")
+    app.run(port=8000, host='127.0.0.1')
