d6611075145849538696b5d4fa0f67f10db7b293
protein <taimanova.sveta@gmail.com>
2024-04-26 20:05:34 +0300

    Исправлен шаблон для окна входа. Добавила БД

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ def about():
     return render_template('about.html', title='О нас')
 
 
-@app.route('/registrar')
-def registrar():
-    return render_template('registrar.html', title='Регистрация')
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Вход')
+
+@app.route('/<username>')
+def user(username):
+    return render_template('user.html', title='Вход', name=username)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1', debug=True)
